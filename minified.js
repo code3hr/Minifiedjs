adb318bc25a513632b6bf884da82217f6fff7628
chick joel CJ <chickjoel4@gmail.com>
2022-04-09 11:01:48 +0400

    Update minified.js

--- minified.js
+++ minified.js
@@ -4,6 +4,8 @@ const args = process.argv;
 var file = args[2];
 fs.readFile(file, 'utf8', function(err, data) {
 
+    
+    //this uses regex to remove unwanted spaces from the source code
     data = data.replace(/[\s]+/g, "");
 
     fs.writeFile('min_' + file, data, 'utf-8', function(err) {
